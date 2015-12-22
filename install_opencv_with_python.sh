f8a2e8a2fbb9f1de1305513b9f93ebc6abc15cb5
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-12-21 17:39:45 -0800

    should work

--- install_opencv_with_python.sh
+++ install_opencv_with_python.sh
@@ -6,13 +6,11 @@ cd opencv
 git checkout v2.4.9
 
 # Load the following modules
+cd opencv
 module load gcc
 module load cmake
-module load python/2.7.8
-module load numpy/1.9.0
-
-# compile things
-cd opencv
+module load python/2.7.3
+module load numpy/1.8.1
 mkdir build
 cd build
 export PYTHON_EXECUTABLE=$(readlink -e $(which python))
@@ -21,6 +19,7 @@ export PYTHON_INCLUDE_DIRS=${PYTHON_EXECUTABLE%/*/*}/include/python2.7
 export PYTHON_LIBRARY=${PYTHON_EXECUTABLE%/*/*}/lib/libpython2.7.so
 export PYTHON_NUMPY_INCLUDE_DIR=$NUMPY_DIR/lib/python2.7/site-packages/numpy/core/include/
 export PYTHON_PACKAGES_PATH=install #${PYTHON_EXECUTABLE%/*/*}/lib/python2.7/site-packages
+
 cmake  \
 -DWITH_OPENCL=OFF  \
 -DWITH_OPENCLAMDBLAS=OFF  \
