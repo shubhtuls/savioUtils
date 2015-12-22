eb74ce907e749bf676d84065c055766d49492f45
Saurabh Gupta <saurabhgupta.iitd@gmail.com>
2015-12-21 16:37:11 -0800

    script to install opencv

---
+++ install_opencv_with_python.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -i
+
+# clone opencv
+git clone https://github.com/s-gupta/opencv.git
+cd opencv
+git checkout v2.4.9
+
+# Load the following modules
+module load gcc
+module load cmake
+module load python/2.7.8
+module load numpy/1.9.0
+
+# compile things
+cd opencv
+mkdir build
+cd build
+export PYTHON_EXECUTABLE=$(readlink -e $(which python))
+export PYTHON_INCLUDE_PATH=${PYTHON_EXECUTABLE%/*/*}/include/python2.7
+export PYTHON_INCLUDE_DIRS=${PYTHON_EXECUTABLE%/*/*}/include/python2.7
+export PYTHON_LIBRARY=${PYTHON_EXECUTABLE%/*/*}/lib/libpython2.7.so
+export PYTHON_NUMPY_INCLUDE_DIR=$NUMPY_DIR/lib/python2.7/site-packages/numpy/core/include/
+export PYTHON_PACKAGES_PATH=install #${PYTHON_EXECUTABLE%/*/*}/lib/python2.7/site-packages
+cmake  \
+-DBUILD_DOCS=OFF  \
+-DBUILD_EXAMPLES=OFF  \
+-DBUILD_JPEG=OFF  \
+-DBUILD_OPENEXR=OFF  \
+-DBUILD_PACKAGE=OFF  \
+-DBUILD_PERF_TESTS=OFF  \
+-DBUILD_PNG=OFF  \
+-DBUILD_SHARED_LIBS=OFF  \
+-DBUILD_TBB=OFF  \
+-DBUILD_TESTS=OFF  \
+-DBUILD_TIFF=OFF  \
+-DBUILD_WITH_DEBUG_INFO=OFF  \
+-DBUILD_ZLIB=OFF  \
+-DWITH_1394=ON  \
+-DWITH_EIGEN=ON  \
+-DWITH_FFMPEG=ON  \
+-DWITH_GIGEAPI=ON  \
+-DWITH_GSTREAMER=ON  \
+-DWITH_GTK=ON  \
+-DWITH_IPP=OFF  \
+-DWITH_JASPER=ON  \
+-DWITH_JPEG=ON  \
+-DWITH_LIBV4L=ON  \
+-DWITH_OPENCL=OFF  \
+-DWITH_OPENCLAMDBLAS=OFF  \
+-DWITH_OPENCLAMDFFT=OFF  \
+-DWITH_OPENEXR=OFF  \
+-DWITH_OPENGL=OFF  \
+-DWITH_OPENMP=OFF  \
+-DWITH_OPENNI=OFF  \
+-DWITH_PNG=ON  \
+-DWITH_PVAPI=OFF  \
+-DWITH_QT=OFF  \
+-DWITH_TBB=OFF  \
+-DWITH_TIFF=ON  \
+-DWITH_UNICAP=OFF  \
+-DWITH_V4L=OFF  \
+-DWITH_XIMEA=OFF  \
+-DWITH_XINE=OFF  \
+-DWITH_CUDA=OFF  \
+-DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE  \
+-DPYTHON_INCLUDE_PATH=$PYTHON_INCLUDE_PATH \
+-DPYTHON_LIBRARY=$PYTHON_LIBRARY \
+-DPYTHON_NUMPY_INCLUDE_DIR=$PYTHON_NUMPY_INCLUDE_DIR \
+-DPYTHON_PACKAGES_PATH=$PYTHON_PACKAGES_PATH \
+-DCMAKE_INSTALL_PREFIX=install \
+-Wno-dev ../
+make -j 16
